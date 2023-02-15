05dec8989ed62c5732e5d0f4101cb6c3f1a014df
Kaloqn Todorov <k.todorov2019@pmg-pernik.com>
2023-02-15 08:44:36 +0200

    Added second zad

---
+++ RPP Programirane 2/RPP Programirane 2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace RPP_Programirane_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split();
+                switch (command[0])
+                {
+                    case "Reverse":
+                        Array.Reverse(arr);
+                        break;
+                    case "Distinct":
+                        arr = arr.Distinct().ToArray();
+                        break;
+                    case "Replace":
+                        int index = int.Parse(command[1]);
+                        string newWord = command[2];
+                        arr[index] = newWord;
+                        break;
+                    case "Print":
+                        var result = arr.Where(x => x.StartsWith(command[1])).ToArray();
+                        Console.WriteLine(string.Join(" ", arr));
+                        break;
+                }
+            }
+            Console.WriteLine(string.Join(", ", arr));
+        }
+    }
+}
+
---
+++ RPP Programirane 2/RPP Programirane 3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace RPP_Programirane_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
